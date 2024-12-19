c6cfaf404139179288a415d1486bf3609b37859b
Martin Häcker <mhaecker@mac.com>
2024-12-19 23:31:21 +0100

    Add basic home-manager support

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -12,15 +14,26 @@
       self,
       nix-darwin,
       nixpkgs,
+      home-manager,
     }:
     {
+
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .
       darwinConfigurations."Sokrates" = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.dwt = import ./home.nix;
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
         ];
-        specialArgs = { inherit inputs; };
       };
     };
 }
---
+++ home.nix
@@ -0,0 +1,8 @@
+{ pkgs, inputs, ... }:
+{
+  home.stateVersion = "25.05";
+
+  # let home manager manage itself
+  programs.home-manager.enable = true;
+
+}
