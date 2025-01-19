f06206ef513388040239166a4e9f45ea47a8ac6a
Martin Häcker <mhaecker@mac.com>
2025-01-19 20:40:46 +0100

    Remove users configuration, doesn't do anything

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,5 @@
 { inputs, ... }:
 {
-  users.users.dwt = {
-    name = "dwt";
-    home = "/Users/dwt";
-  };
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
