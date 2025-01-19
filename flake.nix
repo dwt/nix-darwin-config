1faede5829cc52acf2e5c3d1d6f119afadfdedec
Martin Häcker <mhaecker@mac.com>
2025-01-19 20:41:21 +0100

    Update to current lix version

--- flake.nix
+++ flake.nix
@@ -2,19 +2,25 @@
   description = "mkk nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # nixpkgs-24.11-darwin
+    lix-module.url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
+    lix-module.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.url = "github:LnL7/nix-darwin"; # nix-darwin-24.11
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-rosetta-builder.url = "github:cpick/nix-rosetta-builder";
+    nix-rosetta-builder.inputs.nixpkgs.follows = "nixpkgs";
+    # home-manager.url = "github:nix-community/home-manager";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     inputs@{
       self,
-      nix-darwin,
       nixpkgs,
-      home-manager,
+      lix-module,
+      nix-darwin,
+      nix-rosetta-builder,
+    # home-manager,
     }:
     {
       # Build darwin flake using:
@@ -23,17 +29,27 @@
         system = "aarch64-darwin";
         specialArgs = { inherit inputs; };
         modules = [
+          lix-module.nixosModules.default
           ./configuration.nix
           ./modules/mkk-kerberos-browsers.nix
           ./modules/sudo-with-biometrics.nix
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.dwt = import ./home.nix;
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
+          # An existing Linux builder is needed to initially bootstrap `nix-rosetta-builder`.
+          # If one isn't already available: comment out the `nix-rosetta-builder` module below,
+          # uncomment this `linux-builder` module, and run `darwin-rebuild switch`:
+          # { nix.linux-builder.enable = true; }
+          # Then: uncomment `nix-rosetta-builder`, remove `linux-builder`, and `darwin-rebuild switch`
+          # a second time. Subsequently, `nix-rosetta-builder` can rebuild itself.
+          { nix-rosetta-builder.onDemand = true; }
+          nix-rosetta-builder.darwinModules.default
+
+          # home-manager.darwinModules.home-manager
+          # {
+          #   home-manager.useGlobalPkgs = true;
+          #   home-manager.useUserPackages = true;
+          #   home-manager.users.dwt = import ./home.nix;
+          #   # Optionally, use home-manager.extraSpecialArgs to pass
+          #   # arguments to home.nix
+          # }
         ];
       };
     };
