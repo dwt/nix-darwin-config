5a62221c2e95e2d45c159f87f7fe015396f707f7
Martin Häcker <mhaecker@mac.com>
2025-02-07 09:00:17 +0100

    Migrate a bunch of tools from homebrew to nix

--- modules/tools-and-project-suport.nix
+++ modules/tools-and-project-suport.nix
@@ -7,6 +7,12 @@
   environment.systemPackages = with pkgs; [
     direnv # .envrc automatic project configuration
     devenv # friendlier nix project configuration
+
     curlie # curl with httpie interface
+
+    # TODO add fish configuration for these
+    fzf # fuzzy finder with integration into many tools
+    starship # cross shell prompt
+    carapace # multi shell completion library
   ];
 }
