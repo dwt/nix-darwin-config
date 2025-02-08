d0993c5e44baa20b02c1e46d2ed055107dff9f5f
Martin Häcker <mhaecker@mac.com>
2025-02-08 15:45:41 +0100

    Extract generic system settings

--- configuration.nix
+++ configuration.nix
@@ -5,6 +5,7 @@
     ./modules/mkk-kerberos-browsers.nix
 
     # general system settings
+    ./modules/nix-and-system-setup.nix
     ./modules/sudo-with-biometrics.nix
     ./modules/nix-linux-rosetta-builder.nix
     ./modules/enable-fish-support.nix
@@ -14,52 +15,9 @@
     ./modules/tools-and-project-suport.nix
   ];
 
-  # Some finder settings
-  system.defaults.finder = {
-    AppleShowAllExtensions = true;
-    FXPreferredViewStyle = "clmv";
-  };
 
   # homebrew = {
   #   enable = true;
   # };
 
-  # Prevent warning because of https://github.com/LnL7/nix-darwin/issues/145
-  nix.channel.enable = false;
-
-  # Enhance nix default setup for flakes and lix
-  nix.settings = {
-    experimental-features = "nix-command flakes";
-    # TODO is this really neccessary? I probably just want the default.
-    bash-prompt-prefix = "(nix:$name)\040";
-    substituters = [
-      "https://cache.nixos.org"
-      "https://cache.lix.systems"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-    ];
-    extra-nix-path = "nixpkgs=flake:nixpkgs";
-    trusted-users = [
-      "dwt"
-    ];
-    extra-platforms = [
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-  };
-
-  # automatically prune no longer needed nix packages
-  nix.gc.automatic = true;
-
-  # Set Git commit hash for darwin-version.
-  system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
-
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 5;
-
-  # The platform the configuration will be used on.
-  nixpkgs.hostPlatform = "aarch64-darwin";
 }
---
+++ modules/nix-and-system-setup.nix
@@ -0,0 +1,47 @@
+{ inputs, ... }:
+{
+  # Some finder settings
+  system.defaults.finder = {
+    AppleShowAllExtensions = true;
+    FXPreferredViewStyle = "clmv";
+  };
+
+  # Prevent warning because of https://github.com/LnL7/nix-darwin/issues/145
+  nix.channel.enable = false;
+
+  # Enhance nix default setup for flakes and lix
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+    # TODO is this really neccessary? I probably just want the default.
+    bash-prompt-prefix = "(nix:$name)\040";
+    substituters = [
+      "https://cache.nixos.org"
+      "https://cache.lix.systems"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+    ];
+    extra-nix-path = "nixpkgs=flake:nixpkgs";
+    trusted-users = [
+      "dwt"
+    ];
+    extra-platforms = [
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  };
+
+  # automatically prune no longer needed nix packages
+  nix.gc.automatic = true;
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+}
