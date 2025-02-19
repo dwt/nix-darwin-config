02e08822569049360738b43ff6f7411240f04f66
Martin Häcker <mhaecker@mac.com>
2025-02-19 21:46:58 +0100

    Cleanup

--- modules/enable-fish-support.nix
+++ modules/enable-fish-support.nix
@@ -3,9 +3,11 @@
 
   # Enable alternative shell support in nix-darwin.
   programs.fish.enable = true;
-  programs.fish.vendor.completions.enable = true;
-  programs.fish.vendor.config.enable = true;
-  programs.fish.vendor.functions.enable = true;
+  programs.fish.vendor = {
+    config.enable = true;
+    completions.enable = true;
+    functions.enable = true;
+  };
 
   # This only enables you to choose the new login shell in
   # -> SystemSettings -> Users & Groups -> Right click on users
