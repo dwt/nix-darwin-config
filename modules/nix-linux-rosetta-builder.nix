75e624a844809b2a68d2ea94238bc958e556faea
Martin Häcker <mhaecker@mac.com>
2025-02-25 20:39:46 +0100

    Needed more ram to build some derivations

--- modules/nix-linux-rosetta-builder.nix
+++ modules/nix-linux-rosetta-builder.nix
@@ -25,4 +25,5 @@
   # This way it auto shuts down after 3h inactivity,
   # while the delay on first launch is almost neglible with about 5-10s.
   nix-rosetta-builder.onDemand = true;
+  nix-rosetta-builder.memory = "8GiB";
 }
