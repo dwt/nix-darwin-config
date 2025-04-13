a55cf130a733a9ca9821c30ecda2e8c4ed52c731
‮rekcäH nitraM‮ <spamfaenger@gmx.de>
2025-04-13 19:57:26 +0200

    Add yt-dlp
    I took the full version so it brings ffmpeg and can transcode audio with
    it.

--- modules/gaming-and-entertainment.nix
+++ modules/gaming-and-entertainment.nix
@@ -1,11 +1,13 @@
 {
+  pkgs,
   pkgs-unstable,
   # pkgs-master,
   ...
 }:
 {
   environment.systemPackages = [
-    pkgs-unstable.ut1999
+    pkgs-unstable.ut1999 # The original Unreal Tournament
+    pkgs.yt-dlp # Download movies / audio from almost all websites
   ];
   nixpkgs.allowUnfreePackages = [
     "ut1999"
