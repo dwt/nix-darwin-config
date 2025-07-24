800c0e09ac667226f6e42850f0c9ac805c114eea
‮rekcäH nitraM‮ <spamfaenger@gmx.de>
2025-07-24 15:45:10 +0200

    Add fortune

--- modules/gaming-and-entertainment.nix
+++ modules/gaming-and-entertainment.nix
@@ -8,6 +8,7 @@
   environment.systemPackages = [
     pkgs-unstable.ut1999 # The original Unreal Tournament
     pkgs.yt-dlp # Download movies / audio from almost all websites
+    pkgs.fortune # Show a random quote on login
   ];
   nixpkgs.allowUnfreePackages = [
     "ut1999"
