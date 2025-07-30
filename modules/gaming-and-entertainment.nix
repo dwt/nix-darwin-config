cb90aa0c9a920d394c3b44991496787e96727c9b
Martin Häcker <spamfaenger@gmx.de>
2025-07-30 21:10:22 +0200

    Disable ut1999 - broken in nixpgks for macos

--- modules/gaming-and-entertainment.nix
+++ modules/gaming-and-entertainment.nix
@@ -6,7 +6,8 @@
 }:
 {
   environment.systemPackages = [
-    pkgs-unstable.ut1999 # The original Unreal Tournament
+    # Waiting for https://github.com/NixOS/nixpkgs/pull/429661
+    # pkgs-unstable.ut1999 # The original Unreal Tournament
     pkgs.yt-dlp # Download movies / audio from almost all websites
     pkgs.fortune # Show a random quote on login
   ];
