0c26f6827c477950d3692709e6458d7e2fe9421e
Martin Häcker <spamfaenger@gmx.de>
2025-09-22 23:02:38 +0200

    Add nurl
    This tool helps create new packages as it prepares the full fetcher call

--- modules/nix-development-and-debugging.nix
+++ modules/nix-development-and-debugging.nix
@@ -32,6 +32,7 @@ in
       nh # nicer frontend for search, build + diff and garbage collection $ nh search hello
       hydra-check # check build status of packages in hydra $ hydra-check hello --arch x86_64-darwin
       nix-init # create initial nix package from project url
+      nurl # get the fetcher call with all arguments for a given url. Supports almost all fetchers.
     ];
   };
 }
