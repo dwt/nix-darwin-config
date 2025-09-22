3be2aede6b2adc011eaf0cf89d5bae07c039de13
Martin Häcker <spamfaenger@gmx.de>
2025-09-22 23:04:42 +0200

    Switch to lix from stable branch, now that the backport was successfull

--- modules/lix.nix
+++ modules/lix.nix
@@ -8,22 +8,21 @@
   # https://lix.systems/add-to-config/#advanced-change
   # see there for advanced nix -> lix overrides of custom packages
   # also supports: stable, latest, git
-  nix.package = pkgs-unstable.lixPackageSets.stable.lix;
-  #pkgs.lixPackageSets = pkgs-unstable.lixPackageSets;
+  nix.package = pkgs.lixPackageSets.latest.lix;
 
-  environment.systemPackages = with pkgs-unstable.lixPackageSets.stable; [
+  environment.systemPackages = with pkgs.lixPackageSets.latest; [
     # https://github.com/Mic92/nixpkgs-review
     nixpkgs-review # check pull requests for nixpkgs -> moved to lix module as it needs to be patched to use lix natively
     # nix-update # update nixpkgs packages
   ];
-  programs.direnv.nix-direnv.package = pkgs-unstable.lixPackageSets.stable.nix-direnv;
+  programs.direnv.nix-direnv.package = pkgs.lixPackageSets.latest.nix-direnv;
 
   # I really want to find a way to include all packages that need overrides automatically, and this is not it.
   # https://git.lix.systems/lix-project/lix/issues/989
   # nixpkgs.overlays = [
   #   (final: prev: {
-  #     lix = pkgs-unstable.lixPackageSets.stable.lix;
-  #     inherit (final.lixPackageSets.stable)
+  #     lix = pkgs-unstable.lixPackageSets.latest.lix;
+  #     inherit (final.lixPackageSets.latest)
   #       nixpkgs-review
   #       nix-direnv
   #       # all of these do not trigger infinite recursion
