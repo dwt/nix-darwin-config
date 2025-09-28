68c24c45a7705596e98c63c8742ab1c54d0919a5
Martin Häcker <spamfaenger@gmx.de>
2025-09-28 21:50:21 +0200

    Enable quake3
    now that sandbox=relaxed it builds again

--- modules/gaming-and-entertainment.nix
+++ modules/gaming-and-entertainment.nix
@@ -6,11 +6,21 @@
 }:
 {
   environment.systemPackages = [
-    # pkgs-unstable.ut1999 # The original Unreal Tournament
+    # Tools and fun stuff
     pkgs.yt-dlp # Download movies / audio from almost all websites
+    pkgs.ffmpeg # Video and audio converter and all around swiss army knife
     pkgs.fortune # Show a random quote on login
+
+    # Games
+    # Doesn't like the lix build sandbox right now. :-(
+    # pkgs-unstable.ut1999 # The original Unreal Tournament
+    # mumble doesn't like the sandbox either right now :-(
+    pkgs-unstable.quake3arena-hires # The original deathmatch game
   ];
   nixpkgs.allowUnfreePackages = [
     "ut1999"
+    "quake3-ioquake3-0-unstable-2025-05-15"
+    "quake3arenadata"
+    "quake3-pointrelease"
   ];
 }
