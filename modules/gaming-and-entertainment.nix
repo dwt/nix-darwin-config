e4b1bcba995c0bb77538f9cb2cf932dfbb6e6867
Martin Häcker <spamfaenger@gmx.de>
2026-01-12 11:07:34 +0100

    Simpler syntax

--- modules/gaming-and-entertainment.nix
+++ modules/gaming-and-entertainment.nix
@@ -5,17 +5,17 @@
   ...
 }:
 {
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs; [
     # Tools and fun stuff
-    pkgs.yt-dlp # Download movies / audio from almost all websites
-    pkgs.ffmpeg # Video and audio converter and all around swiss army knife, required for yt-dlp to extract audio
-    pkgs.fortune # Show a random quote on login
+    yt-dlp # Download movies / audio from almost all websites
+    ffmpeg # Video and audio converter and all around swiss army knife, required for yt-dlp to extract audio
+    fortune # Show a random quote on login
 
     # Games
     # Doesn't like the lix build sandbox right now. :-(
-    pkgs-unstable.ut1999 # The original Unreal Tournament
+    ut1999 # The original Unreal Tournament
     # mumble doesn't like the sandbox either right now :-(
-    pkgs-unstable.quake3arena-hires # The original deathmatch game
+    quake3arena-hires # The original deathmatch game
   ];
   nixpkgs.allowUnfreePackages = [
     "ut1999"
