f85f2db1b39e7e09494f6e718cf29eb9d79a7f1c
Martin Häcker <spamfaenger@gmx.de>
2026-02-10 20:57:03 +0100

    Switch to upstream allowUnfrePackages

--- modules/gaming-and-entertainment.nix
+++ modules/gaming-and-entertainment.nix
@@ -17,7 +17,7 @@
     # mumble doesn't like the sandbox either right now :-(
     quake3arena-hires # The original deathmatch game
   ];
-  nixpkgs.allowUnfreePackages = [
+  nixpkgs.config.allowUnfreePackages = [
     "ut1999"
     "quake3-ioquake3-0-unstable-2025-05-15"
     "quake3arenadata"
--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -5,30 +5,8 @@
   ...
 }:
 {
-  # TODO contribute allowUnfreePackages to nixpkgs
-  options = with lib; {
-    nixpkgs.allowUnfreePackages = mkOption {
-      type = with types; listOf str;
-      default = [ ];
-      example = [ "ut1999" ];
-      description = ''
-        Allows specific unfree packages to be used.
-
-        This option overrides `nixpkgs.config.allowUnfreePredicate`with a function that permits the listed package names.
-
-        Unlike `nixpkgs.config.allowUnfreePredicate`, this option merges additively, similar to `environment.systemPackages`.
-        This enables defining allowed unfree packages in multiple modules, close to where they are used.
-
-        This avoids the need to centralize all unfree package declarations or globally enable unfree packages via
-        `nixpkgs.config.allowUnfree = true`.
-      '';
-    };
-  };
 
   config = {
-    nixpkgs.config.allowUnfreePredicate = lib.mkIf (
-      builtins.length config.nixpkgs.allowUnfreePackages > 0
-    ) (pkg: builtins.elem (lib.getName pkg) config.nixpkgs.allowUnfreePackages);
 
     # provide my custom nixpkgs versions as optional module args
     _module.args =
